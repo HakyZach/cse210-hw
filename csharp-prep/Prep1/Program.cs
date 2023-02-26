f2bc190c8cdaeaa7bcad207c558fef520792ac02
Zachary Scieneaux <zscieneaux@sbcglobal.net>
2023-02-25 20:13:07 -0700

    prepare content

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,13 @@
 using System;
-
-class Program
-{
+class Program{
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        string f = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string l = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {l}, {f} {l}.");
     }
 }
